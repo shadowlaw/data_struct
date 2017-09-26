8a2e5fb47d6b41de5878ef22ae046adf394f119d
javed <jdwright870@gmail.com>
2017-09-25 19:44:34 -0500

    Refactored search and added insert to BST

--- data_structures.py
+++ data_structures.py
@@ -5,36 +5,51 @@ class Node:
 
 class BST:
     def __init__(self, value=None):
-        self.root = Node(value)
-        self.left = None
-        self.right = None
+        if value is None:
+            self.root = None
+            self.left = None
+            self.right = None
+        else:
+            self.root = Node(value)
+            self.left = None
+            self.right = None
 
     def search(self, value):
         if value == self.root.value:
             return self.root, "found"
 
-        if self.right is not None:
-            if value > self.root.value:
+        if value > self.root.value:
+            if self.right is not None:
                 return self.right.search(value)
-        else:
-            return self, 'right'
+            else:
+                return self, 'right'
 
-        if self.left is not None:
-            if value < self.root.value:
+        if value < self.root.value:
+            if self.left is not None:
                 return self.left.search(value)
-        else:
-            return self, 'left'
+            else:
+                return self, 'left'
 
-bst = BST(2)
-bst.right = BST(3)
-bst.left = BST(1)
-bst.right.right = BST(9)
-bst.right.left = BST(4)
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+            return
 
-print(bst.search(9)[0].value)
+        node, status= self.search(value)
 
-assert bst.root.value == 2
-assert bst.left.root.value == 1
-assert bst.right.root.value == 3
-assert bst.right.right.root.value == 9
-assert bst.right.left.root.value == 4
+        if status == "found":
+            node.value = value
+        elif status == 'right':
+            node.right = BST(value)
+        elif status == 'left':
+            node.left = BST(value)
+
+
+bst = BST()
+bst.insert(2)
+bst.insert(3)
+bst.insert(1)
+bst.insert(9)
+bst.insert(4)
+bst.insert(-1)
+
