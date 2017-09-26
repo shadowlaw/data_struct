f7c30c7403d6790f2aefd65b4cf95a78a9fbe856
javed <jdwright870@gmail.com>
2017-09-25 20:56:42 -0500

    Added comments to BST data structure.

--- data_structures.py
+++ data_structures.py
@@ -1,10 +1,24 @@
 class Node:
+    '''Node class, holds a value for the BST data structure.'''
+
     def __init__(self, value = None):
+        '''Initializes value attribute for node
+
+        :param
+        value - initialized to none. Assigned instance variable to the hold node value.
+        '''
         self.value = value
 
 
 class BST:
+    '''Binary search Tree class. Implements a simple binary search tree.'''
+
     def __init__(self, value=None):
+        '''Constructor for BST.
+
+        :param
+        value - initialized to none. Added to the  BST root node.
+        '''
         if value is None:
             self.root = None
             self.left = None
@@ -15,6 +29,15 @@ class BST:
             self.right = None
 
     def search(self, value):
+        '''Locates the passed value.
+
+        :param
+        value - value to be located.
+
+        :returns
+        tuple containing node and node location for value to be placed.
+        '''
+
         if value == self.root.value:
             return self.root, "found"
 
@@ -31,11 +54,17 @@ class BST:
                 return self, 'left'
 
     def insert(self, value):
+        '''inserts a value into the BST using the search method.
+
+        :param
+        value - value to be inserted
+        
+        '''
         if self.root is None:
             self.root = Node(value)
             return
 
-        node, status= self.search(value)
+        node, status = self.search(value)
 
         if status == "found":
             node.value = value
