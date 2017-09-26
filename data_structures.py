336417e235bd53673b914c118a2a171803520dbb
javed <jdwright870@gmail.com>
2017-09-25 19:16:43 -0500

    Refactored bst search method

--- data_structures.py
+++ data_structures.py
@@ -1,54 +1,40 @@
+class Node:
+    def __init__(self, value = None):
+        self.value = value
+
 
 class BST:
-    class Node:
-        def __init__(self, value = None):
-            self.value = value
-            self.left = None
-            self.right = None
-
     def __init__(self, value=None):
-        self.root = BST.Node(value)
+        self.root = Node(value)
+        self.left = None
+        self.right = None
 
     def search(self, value):
         if value == self.root.value:
-            return self.root, 'found'
-        elif value < self.root.value:
-            if self.root.left is not None:
-                return self.root.left.search(value)
-            else:
-                return self.root, 'left'
-        elif value > self.root.value:
-            if self.root.right is not None:
-                return self.root.right.search(value)
-            else:
-                return self.root, 'right'
+            return self.root, "found"
 
-    def insert(self, value):
-        if self.root.value is None:
-            self.root.value = BST(value)
-            return
+        if self.right is not None:
+            if value > self.root.value:
+                return self.right.search(value)
+        else:
+            return self, 'right'
 
-        insertion_point = self.search(value)
+        if self.left is not None:
+            if value < self.root.value:
+                return self.left.search(value)
+        else:
+            return self, 'left'
 
-        if insertion_point[1] == 'found':
-            insertion_point[0].value = BST(value)
-        elif insertion_point[1] == 'left':
-            insertion_point[0].left = BST(value)
-        elif insertion_point[1] == 'right':
-            insertion_point[0].right = BST(value)
+bst = BST(2)
+bst.right = BST(3)
+bst.left = BST(1)
+bst.right.right = BST(9)
+bst.right.left = BST(4)
 
-'''last test notes
-    - insertion from constructor builds proper tree
-    - insertion into empty tree builds deformed tree
-    - refactor insertion method.
-'''
-
-bst = BST()
-bst.insert(2)
-bst.insert(1)
-bst.insert(3)
+print(bst.search(9)[0].value)
 
 assert bst.root.value == 2
-assert bst.root.left.root.value == 1
-assert bst.root.right.root.value == 3
-
+assert bst.left.root.value == 1
+assert bst.right.root.value == 3
+assert bst.right.right.root.value == 9
+assert bst.right.left.root.value == 4
