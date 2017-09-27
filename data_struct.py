206338cda2170e78f85fbf75f69cb1fea5481006
javed <jdwright870@gmail.com>
2017-09-27 18:30:40 -0500

    Added is_empty, is_leaf, added fuction frame for successor and delete_node

--- data_struct.py
+++ data_struct.py
@@ -1,3 +1,10 @@
+'''Implements data basic structures
+
+Data Structures implemented:
+- Binary Search Tress (BST - Node class required for use.)
+- Red Black Trees (RBT - Node class requited for use.)
+=[
+'''
 class Node:
     '''Node class, holds a value for the BST data structure.'''
 
@@ -28,6 +35,16 @@ class BST:
             self.left = None
             self.right = None
 
+    def is_empty(self):
+        '''Returns True if the tree is empty, false otherwise.'''
+
+        return self.root == None and self.left == None and self.right == None
+
+    def is_leaf(self):
+        '''Returns true if node is a leaf, false otherwise'''
+
+        return self.left == None and self.right == None
+
     def search(self, value):
         '''Locates the passed value.
 
@@ -37,6 +54,9 @@ class BST:
         :returns
         tuple containing node and node location for value to be placed.
         '''
+
+        if self.is_empty():
+            return Node(), 'Empty tree'
 
         if value == self.root.value:
             return self.root, "found"
@@ -54,13 +74,14 @@ class BST:
                 return self, 'left'
 
     def insert(self, value):
-        '''inserts a value into the BST using the search method.
+        '''Inserts a value into the BST using the search method.
 
         :param
         value - value to be inserted
 
         '''
-        if self.root is None:
+
+        if self.is_empty():
             self.root = Node(value)
             return
 
@@ -72,3 +93,34 @@ class BST:
             node.right = BST(value)
         elif status == 'left':
             node.left = BST(value)
+
+    def successor(self, node):
+        '''Locates tree successor for node passed.
+
+        :param
+        node - node for which to find successor.
+
+        :return
+        returns successor for node passed, false otherwise.
+        '''
+        pass
+
+
+    def delete_node(self, value):
+        '''Locates node to be removed.
+
+        :param
+        value - value of the node to be removed
+
+        :return
+        returns true on successful deletion, false if node is not found.
+        '''
+
+        def remove_node(parent_node, indicator):
+            '''Removes node from bst parent node instance.
+
+            :param
+            parent_node - parent node of node to be removed.
+            indicator - indicates either left or right node to be removed from parent node.
+            '''
+            pass
