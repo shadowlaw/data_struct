683f184da9eae1b7b60bd4774f84fdf1e609da7f
Javed Wright <jdwright870@gmail.com>
2017-10-12 14:33:02 -0500

    Added is_root, is_empty, is_leaf.

--- BinaryHeap.py
+++ BinaryHeap.py
@@ -6,12 +6,18 @@ class Node:
 
 class Heap:
     def __init__(self, value=None):
-        if value is None:
-            self.root = None
-            self.left = None
-            self.right = None
-        else:
-            self.root = Node(value)
-            self.left = None
-            self.right = None
+        self.root = Node(value)
+        self.left = None
+        self.right = None
 
+    def is_empty(self):
+        return self.root.value is None
+
+    def is_leaf(self):
+        return self.root.value is not None and self.left is None and self.right is None
+
+    def is_root(self):
+        return self.root.is_min is True
+
+    def insert(self):
+        pass
